ed33d8598b260e720bdb97ec9a3f8923f63d59b0
YannisMarketakis <ymark_em@yahoo.gr>
2015-11-02 17:21:41 +0200

    first (untested) implementation of DirectorySearch SOAP methods

--- DataServices-middleware/src/main/java/eu/lifewatch/dataservices/middleware/directoryservice/DirectoryService_Search.java
+++ DataServices-middleware/src/main/java/eu/lifewatch/dataservices/middleware/directoryservice/DirectoryService_Search.java
@@ -1,11 +1,18 @@
 package eu.lifewatch.dataservices.middleware.directoryservice;
 
+import eu.lifewatch.core.impl.VirtuosoRepositoryManager;
 import eu.lifewatch.core.model.DirectoryStruct;
 import eu.lifewatch.core.model.Triple;
+import eu.lifewatch.exception.QueryExecutionException;
+import eu.lifewatch.service.impl.DirectoryService;
+import java.util.ArrayList;
 import java.util.List;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import org.apache.log4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * DirectoryService-Search provides the functionality to search of the the Directory 
@@ -19,29 +26,81 @@ import javax.jws.WebParam;
  */
 @WebService(serviceName = "DirectoryService_Search")
 public class DirectoryService_Search {
-
+    private static final Logger logger=Logger.getLogger(DirectoryService_Search.class);
 
     @WebMethod(operationName = "searchDataset")
-    public List<DirectoryStruct> searchDataset(@WebParam(name = "datasetName") String datasetName, @WebParam(name = "ownerName") String ownerName, @WebParam(name = "datasetURI") String datasetURI, @WebParam(name = "datasetType") String datasetType, @WebParam(name = "repositoryGraph") String repositoryGraph) {
-        //TODO write your implementation code here:
-        return null;
+    public List<DirectoryStruct> searchDataset(@WebParam(name = "datasetName") String datasetName,
+                                               @WebParam(name = "ownerName") String ownerName,
+                                               @WebParam(name = "datasetURI") String datasetURI, 
+                                               @WebParam(name = "datasetType") String datasetType, 
+                                               @WebParam(name = "repositoryGraph") String repositoryGraph) {
+        logger.info("Request for searchDataset("+datasetName+","+ownerName+","+datasetURI+","+datasetType+","+repositoryGraph+")");
+        List<DirectoryStruct> retList=new ArrayList<>();
+        try{
+            ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+            VirtuosoRepositoryManager repoManager=context.getBean(VirtuosoRepositoryManager.class);
+            DirectoryService api=new DirectoryService(repoManager);
+            retList=api.searchDataset(datasetName, ownerName, datasetURI, datasetType, repositoryGraph);
+            logger.info("Number of results that will be returned: "+retList.size());
+        }catch(QueryExecutionException ex){
+            logger.error("An error occured while searching for resources. Returning an empty list.\n", ex);
+        }
+        return retList;
     }
-
-    @WebMethod(operationName = "searchDataset_1")
-    public List<DirectoryStruct> searchDataset(@WebParam(name = "datasetName") String datasetName, @WebParam(name = "ownerName") String ownerName, @WebParam(name = "datasetURI") String datasetURI, @WebParam(name = "datasetType") String datasetType, @WebParam(name = "limit") int limit, @WebParam(name = "offset") int offset, @WebParam(name = "repositoryGraph") String repositoryGraph) {
-        //TODO write your implementation code here:
-        return null;
+    
+    @WebMethod(operationName = "searchDataset")
+    public List<DirectoryStruct> searchDataset(@WebParam(name = "datasetName") String datasetName,
+                                               @WebParam(name = "ownerName") String ownerName,
+                                               @WebParam(name = "datasetURI") String datasetURI, 
+                                               @WebParam(name = "datasetType") String datasetType, 
+                                               @WebParam(name = "limit") int limit, 
+                                               @WebParam(name = "offset") int offset,
+                                               @WebParam(name = "repositoryGraph") String repositoryGraph) {
+        logger.info("Request for searchDataset("+datasetName+","+ownerName+","+datasetURI+","+datasetType+","+limit+","+offset+","+repositoryGraph+")");
+        List<DirectoryStruct> retList=new ArrayList<>();
+        try{
+            ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+            VirtuosoRepositoryManager repoManager=context.getBean(VirtuosoRepositoryManager.class);
+            DirectoryService api=new DirectoryService(repoManager);
+            retList=api.searchDataset(datasetName, ownerName, datasetURI, datasetType, limit, offset, repositoryGraph);
+            logger.info("Number of results that will be returned: "+retList.size());
+        }catch(QueryExecutionException ex){
+            logger.error("An error occured while searching for resources. Returning an empty list.\n", ex);
+        }
+        return retList;
     }
 
     @WebMethod(operationName = "searchResource")
-    public List<Triple> searchResource(@WebParam(name = "resourceURI") String resourceURI, @WebParam(name = "repositoryGraph") String repositoryGraph) {
-        //TODO write your implementation code here:
-        return null;
+    public List<Triple> searchResource(@WebParam(name = "resourceURI") String resourceURI,
+                                       @WebParam(name = "repositoryGraph") String repositoryGraph) {
+        logger.info("Request for searchResource("+resourceURI+","+repositoryGraph+")");
+        List<Triple> retList=new ArrayList<>();
+        try{
+            ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+            VirtuosoRepositoryManager repoManager=context.getBean(VirtuosoRepositoryManager.class);
+            DirectoryService api=new DirectoryService(repoManager);
+            retList=api.searchResource(resourceURI, repositoryGraph);
+            logger.info("Number of results that will be returned: "+retList.size());
+        }catch(QueryExecutionException ex){
+            logger.error("An error occured while searching for resources. Returning an empty list.\n", ex);
+        }
+        return retList;
     }
 
     @WebMethod(operationName = "searchLiteral")
-    public List<Triple> searchLiteral(@WebParam(name = "literalValue") String literalValue, @WebParam(name = "repositoryGraph") String repositoryGraph) {
-        //TODO write your implementation code here:
-        return null;
+    public List<Triple> searchLiteral(@WebParam(name = "literalValue") String literalValue,
+                                      @WebParam(name = "repositoryGraph") String repositoryGraph) {
+        logger.info("Request for searchLiteral("+literalValue+","+repositoryGraph+")");
+        List<Triple> retList=new ArrayList<>();
+        try{
+            ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+            VirtuosoRepositoryManager repoManager=context.getBean(VirtuosoRepositoryManager.class);
+            DirectoryService api=new DirectoryService(repoManager);
+            retList=api.searchLiteral(literalValue, repositoryGraph);
+            logger.info("Number of results that will be returned: "+retList.size());
+        }catch(QueryExecutionException ex){
+            logger.error("An error occured while searching for literals. Returning an empty list.\n", ex);
+        }
+        return retList;
     }
 }
