5007c1cffe80c4f40d780653d0d2513cb3ac6f36
YannisMarketakis <ymark_em@yahoo.gr>
2015-11-02 10:46:47 +0200

    fixed imports and removed unwanted comments

--- DataServices-middleware/src/main/java/eu/lifewatch/dataservices/middleware/directoryservice/DirectoryService_Search.java
+++ DataServices-middleware/src/main/java/eu/lifewatch/dataservices/middleware/directoryservice/DirectoryService_Search.java
@@ -1,16 +1,18 @@
 package eu.lifewatch.dataservices.middleware.directoryservice;
 
 import eu.lifewatch.core.model.DirectoryStruct;
+import eu.lifewatch.core.model.Triple;
 import java.util.List;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
-import javax.xml.ws.RequestWrapper;
-import javax.xml.ws.ResponseWrapper;
 
 /**
  * DirectoryService-Search provides the functionality to search of the the Directory 
- * using various ways (search for specific structs, or triples in general)
+ * using various ways (search for specific structs, or triples in general). 
+ * For more information about the usage of the SOAP services and the contents and 
+ * manipulation of the returned structs please refer to the javadoc documentation of the 
+ * DataServices-api.
  *
  * @author Yannis Marketakis (marketak 'at' ics 'dot' forth 'dot' gr)
  * @author Nikos Minadakis (minadakn 'at' ics 'dot' forth 'dot' gr)
@@ -19,38 +21,24 @@ import javax.xml.ws.ResponseWrapper;
 public class DirectoryService_Search {
 
 
-    /**
-     * Web service operation
-     */
     @WebMethod(operationName = "searchDataset")
     public List<DirectoryStruct> searchDataset(@WebParam(name = "datasetName") String datasetName, @WebParam(name = "ownerName") String ownerName, @WebParam(name = "datasetURI") String datasetURI, @WebParam(name = "datasetType") String datasetType, @WebParam(name = "repositoryGraph") String repositoryGraph) {
         //TODO write your implementation code here:
         return null;
     }
 
-    /**
-     * Web service operation
-     */
     @WebMethod(operationName = "searchDataset_1")
-    @RequestWrapper(className = "eu.searchDataset_1")
-    @ResponseWrapper(className = "eu.searchDataset_1Response")
     public List<DirectoryStruct> searchDataset(@WebParam(name = "datasetName") String datasetName, @WebParam(name = "ownerName") String ownerName, @WebParam(name = "datasetURI") String datasetURI, @WebParam(name = "datasetType") String datasetType, @WebParam(name = "limit") int limit, @WebParam(name = "offset") int offset, @WebParam(name = "repositoryGraph") String repositoryGraph) {
         //TODO write your implementation code here:
         return null;
     }
 
-    /**
-     * Web service operation
-     */
     @WebMethod(operationName = "searchResource")
     public List<Triple> searchResource(@WebParam(name = "resourceURI") String resourceURI, @WebParam(name = "repositoryGraph") String repositoryGraph) {
         //TODO write your implementation code here:
         return null;
     }
 
-    /**
-     * Web service operation
-     */
     @WebMethod(operationName = "searchLiteral")
     public List<Triple> searchLiteral(@WebParam(name = "literalValue") String literalValue, @WebParam(name = "repositoryGraph") String repositoryGraph) {
         //TODO write your implementation code here:
