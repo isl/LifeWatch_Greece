2793dd2ecfceb4ca4b914856d87e5eb85fb7a536
YannisMarketakis <ymark_em@yahoo.gr>
2015-11-03 11:41:26 +0200

    implemented all methods for searching from the directory

--- DataServices-middleware-client/src/main/java/eu/lifewatch/dataservices/middleware/client/directory/SearchClient.java
+++ DataServices-middleware-client/src/main/java/eu/lifewatch/dataservices/middleware/client/directory/SearchClient.java
@@ -38,26 +38,96 @@ public class SearchClient {
                                    "\t"+struct.getCreationDate()+
                                    "\t"+struct.getCreationEventURI()+"...");
             }
-        }catch (Exception ex) {
+        }catch(Exception ex) {
             logger.error("An error occured while searching for datasets",ex);
         }
     }
     
     private static void searchDatasetWithinRange(){
-        
+        try{ 
+            DirectoryServiceSearch_Service service = new DirectoryServiceSearch_Service();
+            DirectoryServiceSearch port = service.getDirectoryServiceSearchPort();
+            String datasetName = "dataset";
+            String ownerName = "owner";
+            String datasetURI = "http://localhost/directory/dataset1";
+            String datasetType = "dataset type";
+            int limit = 1;
+            int offset = 0;
+            String repositoryGraph = "http://www.ics.forth.gr/isl/lifewatch/directory";
+            System.out.println("Searching for datasets with the following details: "+
+                               "\tDataset Name: "+datasetName+
+                               "\tOwner Name: "+ownerName+
+                               "\tDataset URI: "+datasetURI+
+                               "\tDataset Type: "+datasetType+
+                               "\tLimit: "+limit+
+                               "\tOffset: "+offset+
+                               "\tGraphspace: "+repositoryGraph);
+            List<DirectoryStruct> results = port.searchDatasetWithinRange(datasetName, ownerName, datasetURI, datasetType, limit, offset, repositoryGraph);
+            System.out.println("Found "+results.size()+" datasets");
+            for(DirectoryStruct struct : results){
+                System.out.println(struct.getDatasetID()+
+                                   "\t"+struct.getDatasetName()+
+                                   "\t"+struct.getOwnerName()+
+                                   "\t"+struct.getOwnerURI()+
+                                   "\t"+struct.getCuratorName()+
+                                   "\t"+struct.getCuratorURI()+
+                                   "\t"+struct.getCreationDate()+
+                                   "\t"+struct.getCreationEventURI()+"...");
+            }
+        }catch(Exception ex) {
+            logger.error("An error occured while searching for datasets",ex);
+        }
     }
     
     private static void searchResource(){
-        
+        try{
+            DirectoryServiceSearch_Service service = new DirectoryServiceSearch_Service();
+            DirectoryServiceSearch port = service.getDirectoryServiceSearchPort();
+            java.lang.String resourceURI = "http://localhost/directory/dataset1";
+            java.lang.String repositoryGraph = "http://www.ics.forth.gr/isl/lifewatch/directory";
+            System.out.println("Searching for resources with the following details: "+
+                               "\tResource URI: "+resourceURI+
+                               "\tGraphspace: "+repositoryGraph);
+            List<Triple> results = port.searchResource(resourceURI, repositoryGraph);
+            System.out.println("Found "+results.size()+" triples containing the resource");
+            for(Triple triple : results){
+                System.out.println("Subject: "+triple.getSubject()+
+                                   "\tPredicate: "+triple.getPredicate()+
+                                   "\tObject: "+triple.getObject());
+            }
+        } catch (Exception ex) {
+            logger.error("An error occured while searching for triples containing a resource",ex);
+        }
     }
     
     private static void searchLiteral(){
-        
+        try{ 
+            DirectoryServiceSearch_Service service = new DirectoryServiceSearch_Service();
+            DirectoryServiceSearch port = service.getDirectoryServiceSearchPort();
+            String literalValue = "owner";
+            String repositoryGraph = "http://www.ics.forth.gr/isl/lifewatch/directory";
+            System.out.println("Searching for literals with the following details: "+
+                               "\tResource URI: "+literalValue+
+                               "\tGraphspace: "+repositoryGraph);
+            List<Triple> results = port.searchLiteral(literalValue, repositoryGraph);
+            System.out.println("Found "+results.size()+" triples containing the literal");
+            for(Triple triple : results){
+                System.out.println("Subject: "+triple.getSubject()+
+                                   "\tPredicate: "+triple.getPredicate()+
+                                   "\tObject: "+triple.getObject());
+            }
+        } catch (Exception ex) {
+            logger.error("An error occured while searching for triples containing a literal",ex);
+        }
     }
     
     public static void main(String[] args){
         searchDataset();
         Commons.printSeparator();
+        searchDatasetWithinRange();
+        Commons.printSeparator();
+        searchResource();
+        Commons.printSeparator();
+        searchLiteral();
     }
-    
 }
