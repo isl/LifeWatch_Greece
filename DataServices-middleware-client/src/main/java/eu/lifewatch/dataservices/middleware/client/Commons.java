0e0201040a8343a8bb7355bc11148369984e2232
YannisMarketakis <ymark_em@yahoo.gr>
2015-11-03 11:06:44 +0200

    added a class for adding commonly used methods

---
+++ DataServices-middleware-client/src/main/java/eu/lifewatch/dataservices/middleware/client/Commons.java
@@ -0,0 +1,11 @@
+package eu.lifewatch.dataservices.middleware.client;
+
+/**
+ * @author Yannis Marketakis (marketak 'at' ics 'dot' forth 'dot' gr)
+ * @author Nikos Minadakis (minadakn 'at' ics 'dot' forth 'dot' gr)
+ */
+public class Commons {
+    public static void printSeparator(){
+        System.out.println("--------------------------------------------");
+    }
+}
