126e8690458d17bb07f9b32a2b922f5c4de272ec
YannisMarketakis <ymark_em@yahoo.gr>
2015-11-03 12:11:27 +0200

    added a client for InsertDataset

---
+++ DataServices-middleware-client/src/main/java/eu/lifewatch/dataservices/middleware/client/directory/InsertClient.java
@@ -0,0 +1,68 @@
+package eu.lifewatch.dataservices.middleware.client.directory;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author Yannis Marketakis (marketak 'at' ics 'dot' forth 'dot' gr)
+ * @author Nikos Minadakis (minadakn 'at' ics 'dot' forth 'dot' gr)
+ */
+public class InsertClient {
+    private static final Logger logger=Logger.getLogger(InsertClient.class);
+
+    private static void insertDataset(){
+        try{
+            DirectoryServiceInsert_Service service = new DirectoryServiceInsert_Service();
+            DirectoryServiceInsert port = service.getDirectoryServiceInsertPort();
+            DirectoryStruct struct = new DirectoryStruct();
+            struct.setDatasetURI("http://localhost/directory/dataset_new");
+            struct.setDatasetName("dataset new");
+            struct.setDatasetID("dataset ID new");
+            struct.setDatasetType("dataset type new");
+            struct.setAccessMethod("access method new");
+            struct.setAccessMethodURI("http://localhost/directory/access_method_new");
+            struct.setAccessRights("access rights new");
+            struct.setAccessRightsURI("http://localhost/directory/access_rights_new");
+            struct.setAttributeAssignmentEventURI("http://localhost/directory/attribute_assignment_new");
+            struct.setContactPoint("contact point new");
+            struct.setCreationDate("creation date new");
+            struct.setCreationEventURI("http://localhost/directory/creatio_event_new");
+            struct.setCreatorName("creator name new");
+            struct.setCreatorURI("http://localhost/directory/creator_new");
+            struct.setCuratorName("curator name new");
+            struct.setCuratorURI("http://localhost/directory/curator_new");
+            struct.setDescription("description new");
+            struct.setEmbargoPeriod("embargo period new");
+            struct.setEmbargoState("embargo state new");
+            struct.setImageTitle("image title new");
+            struct.setImageURI("http://localhost/directory/image_new");
+            struct.setKeeperName("keeper name new");
+            struct.setKeeperURI("http://localhost/directory/keeper_new");
+            struct.setLocationURL("http://localhost/directory/location_new");
+            struct.setOwnerName("owner name");
+            struct.setOwnerURI("http://localhost/directory/owner_new");
+            struct.setParentDatasetName("parent dataset new");
+            struct.setParentDatasetURI("http://localhost/directory/parent_dataset_new");
+            struct.setPublicationDate("publication date new");
+            struct.setPublicationEventURI("http://localhost/directory/publication_event_new");
+            struct.setPublisherName("publisher name new");
+            struct.setPublisherURI("http://localhost/directory/publisher_new");
+            struct.setRightsHolderName("rights holder new");
+            struct.setRightsHolderURI("http://localhost/directory/rights_holder_new");
+            String repositoryGraph = "http://www.ics.forth.gr/isl/lifewatch/directory";
+            System.out.println("Adding a new dataset with the following details: "+
+                               "\tDataset Name: "+struct.getDatasetName()+
+                               "\tOwner Name: "+struct.getOwnerName()+
+                               "\tDataset URI: "+struct.getDatasetURI()+
+                               "\tDataset Type: "+struct.getDatasetType()+
+                               "\tGraphspace: "+repositoryGraph+" ...");
+            boolean result = port.insertDataset(struct, repositoryGraph);
+            System.out.println("Insert Dataset results= "+result);
+        } catch (Exception ex) {
+            logger.error("An error occured while adding a new dataset",ex);
+        }
+    }
+    
+    public static void main(String[] args){
+        insertDataset();
+    }
+}
