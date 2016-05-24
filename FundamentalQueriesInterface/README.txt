fe835f01cee928c1f44a1dab58eb166a9f778b21
YannisMarketakis <ymark_em@yahoo.gr>
2016-05-24 17:20:23 +0300

    import of the FQI sources

---
+++ FundamentalQueriesInterface/README.txt
@@ -0,0 +1,8 @@
+README
+
+1.Make start.sh executable
+	chmod +x start.sh
+2.Start platform
+	sh start.sh
+3. Open http://localhost:10214/ in your browser
+4. Login with admin:admin (please refer to /resource/Help:BasicSystemConfiguration in order to change the standard login).
---
+++ FundamentalQueriesInterface/start.bat
@@ -0,0 +1,2 @@
+CD jetty-distribution-9.2.11.v20150529
+java -jar start.jar -Djetty.port=xxx -DbackendServerUrl=xxx-Dcom.bigdata.rdf.sail.webapp.ConfigParams.propertyFile=..\RWStore.properties -DrepositoryType=xxx -Dendpoint=xxx -DstartPage=Help:Start -Dcom.metaphacts.config.location=..\config\config.prop -DworkingDir=..\
