ed45e0eaf474685be37df7f5fae020b8a5b5da01
YannisMarketakis <ymark_em@yahoo.gr>
2021-08-11 11:50:19 +0300

    usage examples

---
+++ DataServices-api/src/main/java/eu/lifewatch/example/DirectoryServiceExample.java
@@ -0,0 +1,29 @@
+package eu.lifewatch.example;
+
+import eu.lifewatch.core.model.DirectoryStruct;
+import eu.lifewatch.exception.QueryExecutionException;
+import eu.lifewatch.service.impl.DirectoryService;
+import java.util.List;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * @author Yannis Marketakis (marketak 'at' ics 'dot' forth 'dot' gr)
+ */
+public class DirectoryServiceExample {
+    private static final String GRAPHSPACE_DIRECTORY="http://www.ics.forth.gr/isl/lifewatch/directory";
+    
+    public static void main(String[] args) throws QueryExecutionException{
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        DirectoryService dsManager=context.getBean(DirectoryService.class);
+        
+        List<DirectoryStruct> results=dsManager.searchDataset("", "", "", "sampling", GRAPHSPACE_DIRECTORY);
+        System.out.println("Results: "+results.size());
+        for(DirectoryStruct directoryStruct : results){
+            System.out.println(directoryStruct);
+        }
+    }
+    
+
+}
