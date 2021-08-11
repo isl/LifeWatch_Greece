5d58268703dcb2623ee76d026ed75a4d3252de70
YannisMarketakis <ymark_em@yahoo.gr>
2021-08-11 11:41:01 +0300

    final implementation working for several csv datasets

--- DataServices-api/src/main/java/eu/lifewatch/core/impl/MicroCTHarvester.java
+++ DataServices-api/src/main/java/eu/lifewatch/core/impl/MicroCTHarvester.java
@@ -2,6 +2,7 @@ package eu.lifewatch.core.impl;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
@@ -45,37 +46,80 @@ public class MicroCTHarvester {
         specimenColumnsMappings.put(15, "TaxonomicGroup");
     }
     
+    private static Map<Integer,String> scanningColumnsMappings;
+    static{
+        scanningColumnsMappings=new HashMap<>();
+        scanningColumnsMappings.put(0, "RowID");
+        scanningColumnsMappings.put(1, "SpecimenID");
+        scanningColumnsMappings.put(2, "ScanID");
+        scanningColumnsMappings.put(3, "ContrastEnhancementMethod");
+        scanningColumnsMappings.put(4, "Protocol");
+        scanningColumnsMappings.put(5, "BeginOfPreparationDate");
+        scanningColumnsMappings.put(6, "BeginOfPreparationTime");
+        scanningColumnsMappings.put(7, "EndOfPreparationDate");
+        scanningColumnsMappings.put(8, "EndOfPreparationTime");
+        scanningColumnsMappings.put(9, "PreparationNotes");
+        scanningColumnsMappings.put(10, "ScopeOfScan");
+        scanningColumnsMappings.put(11, "SampleHolder");
+        scanningColumnsMappings.put(12, "ScanningMedium");
+        scanningColumnsMappings.put(13, "ScannedPart");
+        scanningColumnsMappings.put(14, "ScannedBy");
+        scanningColumnsMappings.put(15, "ScanDate");
+        scanningColumnsMappings.put(16, "ScanningDuration");
+        scanningColumnsMappings.put(17, "Instrument");
+        scanningColumnsMappings.put(18, "Voltage_kV");
+        scanningColumnsMappings.put(19, "Current_uA");
+        scanningColumnsMappings.put(20, "Filter");
+        scanningColumnsMappings.put(21, "Zoom_um");
+        scanningColumnsMappings.put(22, "CameraResolution");
+        scanningColumnsMappings.put(23, "Averaging");
+        scanningColumnsMappings.put(24, "RandomMovement");
+        scanningColumnsMappings.put(25, "Scan360or180");
+        scanningColumnsMappings.put(26, "ExposureTime_ms");
+        scanningColumnsMappings.put(27, "OversizeSettings");
+        scanningColumnsMappings.put(28, "ScanNotes");
+        scanningColumnsMappings.put(29, "FileLocation");
+        scanningColumnsMappings.put(30, "ScanFileStatus");
+    }
+    
+    private enum MicroCTResourceType {
+        MicroCT_Specimens,
+        MicroCT_Scanning
+    }
+    
     /** Uses the CSV input file containing MicroCT Specimen resources to construct its equivalent XML output
      * with the proper labels. 
      * 
      * @param input the CSV input
      * @param output the XML output
+     * @param resourceType the type of the MicroCT dataset (i.e. specimens, scanning, etc.)
      * @throws IOException is thrown for any error that might occur while reading writing files
      * @throws ParserConfigurationException is thrown for any error that might occur while exporting data in XML
      * @throws TransformerException is thrown for any error that might occur while exporting data in XML
      * @throws UnsupportedEncodingException is thrown if the input file cannot be read using the defined encoding (i.e. UTF-8) */
-    public static void xmlifyMicroCtSpecimens(File input, File output) throws UnsupportedEncodingException, IOException, ParserConfigurationException, TransformerException{
+    public static void xmlifyMicroCtResources(File input, File output, MicroCTResourceType resourceType) throws UnsupportedEncodingException, IOException, ParserConfigurationException, TransformerException{
         CSVParser csvParser=CSVFormat.DEFAULT.parse(new InputStreamReader(new FileInputStream(input), "UTF-8"));
         Document document=DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
         Element rootElement=document.createElement("root");
         document.appendChild(rootElement);
+        Map<Integer,String> mappingColumnsMap=new HashMap<>();
+        switch(resourceType){
+            case MicroCT_Specimens:
+                mappingColumnsMap=specimenColumnsMappings;
+                break;
+            case MicroCT_Scanning:
+                mappingColumnsMap=scanningColumnsMappings;
+                break;
+        }
+                
         for(CSVRecord record : csvParser){
             Element rowElement=document.createElement("row");
-            for(int i=0;i<specimenColumnsMappings.keySet().size();i++){
-                rowElement.appendChild(createElementWithText(document, specimenColumnsMappings.get(i), record.get(i)));
+            for(int i=0;i<mappingColumnsMap.keySet().size();i++){
+                rowElement.appendChild(createElementWithText(document, mappingColumnsMap.get(i), record.get(i)));
             }
             rootElement.appendChild(rowElement);
         }
         Utils.exportXmlToFile(document, output);
-    }
-    
-    /** Uses the CSV input file containing MicroCT Scanning resources to construct its equivalent XML output
-     * with the proper labels. 
-     * 
-     * @param input the CSV input
-     * @param output the XML output  */
-    public static void xmlifyMicroCtScannings(File input, File output){
-        
     }
     
     private static Element createElementWithText(Document document, String elementName, String text){
@@ -87,6 +131,7 @@ public class MicroCTHarvester {
     }
     
     public static void main(String[] args) throws UnsupportedEncodingException, IOException, ParserConfigurationException, TransformerException{
-        xmlifyMicroCtSpecimens(new File("Specimens.csv"), new File("specimens.xml"));
+        xmlifyMicroCtResources(new File("Y:/Projects/Lifewatch-eric/ENVRI-FAIR/Data/MicroCT/1. Originally Received/Specimens (10).csv"), new File("specimens.xml"),MicroCTResourceType.MicroCT_Specimens);
+        xmlifyMicroCtResources(new File("Y:/Projects/Lifewatch-eric/ENVRI-FAIR/Data/MicroCT/1. Originally Received/Prep_Scan (9).csv"), new File("scanning.xml"),MicroCTResourceType.MicroCT_Scanning);
     }
 }
