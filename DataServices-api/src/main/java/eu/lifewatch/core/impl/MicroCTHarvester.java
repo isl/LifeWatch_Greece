e1a44e24444cf171ebe9fcc1d9d85c4e33e94ff7
YannisMarketakis <ymark_em@yahoo.gr>
2021-08-11 11:42:41 +0300

    removed unused import

--- DataServices-api/src/main/java/eu/lifewatch/core/impl/MicroCTHarvester.java
+++ DataServices-api/src/main/java/eu/lifewatch/core/impl/MicroCTHarvester.java
@@ -2,7 +2,6 @@ package eu.lifewatch.core.impl;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
