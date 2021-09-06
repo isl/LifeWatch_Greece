4a060c020b180c5381814b6a79e361cf055484c5
YannisMarketakis <yannismarketakis@gmail.com>
2021-09-06 16:28:24 +0300

    minor: testing directory structures

--- DataServices-api/src/main/java/eu/lifewatch/example/DirectoryServiceExample.java
+++ DataServices-api/src/main/java/eu/lifewatch/example/DirectoryServiceExample.java
@@ -12,16 +12,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @author Yannis Marketakis (marketak 'at' ics 'dot' forth 'dot' gr)
  */
 public class DirectoryServiceExample {
-    private static final String GRAPHSPACE_DIRECTORY="http://www.ics.forth.gr/isl/lifewatch/directory";
+    private static final String GRAPHSPACE_DIRECTORY="http://www.ics.forth.gr/isl/lifewatch/directory_v9_issue_1";
     
     public static void main(String[] args) throws QueryExecutionException{
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         DirectoryService dsManager=context.getBean(DirectoryService.class);
         
-        List<DirectoryStruct> results=dsManager.searchDataset("Phytoplankton of coastal waters", "", "", "", GRAPHSPACE_DIRECTORY);
+        List<DirectoryStruct> results=dsManager.searchDataset("", "", "http://www.lifewatchgreece.eu/entity/dataset/f97f5c65-9a82-436b-a751-fcd0e9be09cc", "", GRAPHSPACE_DIRECTORY);
         System.out.println("Results: "+results.size());
         for(DirectoryStruct directoryStruct : results){
             System.out.println(directoryStruct);
+            System.out.println(directoryStruct.getAccessRightsURI());
+            System.out.println(directoryStruct.getAccessRights());
+            System.out.println(directoryStruct.getRightsHolderURI());
+            System.out.println(directoryStruct.getRightsHolderName());
         }
     }
     
